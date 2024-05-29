123b417f2af90c3568c01f1f628188d63d52b0bf
jongan.park <jongan.park@samsung.com>
2024-05-29 16:14:23 +0900

    add testcase

--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -5,3 +5,18 @@ TEST(Calc, GopTest_Normal) {
     Calc *calc = new Calc();
     EXPECT_EQ(4, calc->getGop(1, 4));
 }
+
+TEST(Calc, GopTest_Zero) {
+    Calc *calc = new Calc();
+    EXPECT_EQ(4, calc->getGop(1, 4));
+}
+
+TEST(Calc, GopTest_Negetive) {
+    Calc *calc = new Calc();
+    EXPECT_EQ(-4, calc->getGop(-1, 4));
+}
+
+TEST(Calc, GopTest_All_Negetive) {
+    Calc *calc = new Calc();
+    EXPECT_EQ(4, calc->getGop(-1, -4));
+}
