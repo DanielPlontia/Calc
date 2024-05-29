cb3562fd3dc7309bd0149d35387771d497f46f8b
kimtaekun <cjsrnr7532@naver.com>
2024-05-29 16:26:01 +0900

    [FEAT] divide by zero Exception 추가

--- Project5/calc.cpp
+++ Project5/calc.cpp
@@ -1,4 +1,8 @@
+#include <string>
 
 int getDivide(int a, int b) {
+	if (b == 0) {
+		throw std::string("You cannot divide by zero");
+	}
 	return a / b;
 }
--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -5,3 +5,6 @@
 TEST(CalcTest, TestDivide) {
   EXPECT_EQ(2, getDivide(4, 2));
 }
+TEST(CalcTest, TestDivide_byZero) {
+  EXPECT_THROW(getDivide(4, 0), std::string);
+}
