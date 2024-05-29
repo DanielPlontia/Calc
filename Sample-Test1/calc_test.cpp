3dc7f8575c9b14c27585160fe4f30a7a09e6d33a
Heeju <heeju00627@gmail.com>
2024-05-29 16:08:19 +0900

    Edit calc_test: Add getSum test

--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
+#include "../Project5/calc.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(Calc, GetSum) {
+	int expected = 3;
+	int result = getSum(1, 2);
+	EXPECT_EQ(expected, result);
 }
